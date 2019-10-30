29f12bdac1c5b5bee8f21042209729c0d3602ea6
darthmnd <andr.mandy@gmail.com>
2019-10-30 14:48:11 -0300

    fiz uma parte da atividade

--- 01b_ajax_json/index.js
+++ 01b_ajax_json/index.js
@@ -1,3 +1,22 @@
 // [] iterar sobre o array e mostrar as três imagens usando for()
 // [] iterar sobre o array e mostrar as três imagens usando for...of
 // [] iterar sobre o array e mostrar as três imagens usando forEach
+
+
+const req = new XMLHttpRequest;
+req.onreadystatechange = getDados;
+
+function req(){
+    if (req.readyState === 4 && req.status === 200) {
+        const response = request.response;
+        const json = JSON.parse(response);
+        const data = json.data;
+        let obj = data[0]
+    } else {
+        console.log(req.readyState, req.status)
+    }
+}
+
+const url = '.data/dados.json';
+req.open('get', url);
+req.send();
